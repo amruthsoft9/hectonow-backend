84e0a6a3a5975887bf3166dbc4f4b8e1dc2913da
nandu <nandub719@gmail.com>
2025-02-23 16:30:27 +0530

    seller route corrected

--- models/User.js
+++ models/User.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db"); 
-=======
+
 const connection = require("../config/db");
->>>>>>> 2e0db3b61688422186314c0d6d7480308bbabf4f
+
 
 const User = {
   create: (userData, callback) => {
@@ -30,39 +27,6 @@ const User = {
       return callback(null, result[0]); // Return the first user found
     });
   },
-<<<<<<< HEAD
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  cartData: {
-    type: DataTypes.JSON, // ✅ Fixed: Use JSON instead of Object
-    defaultValue: {} // ✅ Fix: Default should be set using defaultValue
-  }
-}, {
-  tableName: "users",
-  timestamps: true
-});
-=======
 };
->>>>>>> 2e0db3b61688422186314c0d6d7480308bbabf4f
 
 module.exports = User;
